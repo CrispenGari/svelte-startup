0260b3efbf6db9d79d2fd67ae355aa732517d955
CrispenGari <Crispengari@gmail.com>
2022-07-12 20:56:04 +0200

    ee

---
+++ 01_GETTING_STARTED/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../styles/global.css";
+  import Footer from "../components/Footer.svelte";
+  import Nav from "../components/Nav.svelte";
+</script>
+
+<svelte:head>
+  <title>My app</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+</svelte:head>
+<Nav />
+<slot />
+<Footer />
--- 01_GETTING_STARTED/src/routes/index.svelte
+++ 01_GETTING_STARTED/src/routes/index.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
   console.log("Hello world");
+
+  let counter = 0;
+  let text = "";
 </script>
 
 <!-- html content goes here -->
 <h1>Hello world</h1>
+<h1>counter: {counter}</h1>
+<h2>{text}</h2>
+<input type="text" placeholder="enter anything..." bind:value={text} />
+<button
+  on:click={() => {
+    counter++;
+  }}
+>
+  increment
+</button>
+
+<button on:click={() => counter--}>decrement</button>
 
 <style>
 </style>
